0140f82565f926583e91b4c77695038f664d2ef0
Paul Tobin <tobster111@gmail.com>
2019-05-05 10:46:17 +0100

    Added redux, store with a reducer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
+import reducer from './components/store/reducer';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(reducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
