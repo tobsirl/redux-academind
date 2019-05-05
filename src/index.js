7c2eda711119db9b716f5bb1f20007a89154baaf
Paul Tobin <tobster111@gmail.com>
2019-05-05 10:54:54 +0100

    Installed react-redux, used Provider to wrap the App component and passed the store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import reducer from './components/store/reducer';
 
@@ -10,5 +11,10 @@ import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(reducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
