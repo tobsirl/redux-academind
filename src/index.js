9fa1184e3ea41f4c9c088a72e1d58f4b1c445643
Paul Tobin <tobster111@gmail.com>
2019-05-08 11:19:12 +0100

    Installed and added redux-thunk

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
 import counterReducer from './components/store/reducers/counter';
 import resultsReducer from './components/store/reducers/results';
@@ -28,7 +29,10 @@ const logger = store => {
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logger, thunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
